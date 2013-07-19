cf4fbdc6b3aa3ea21712d18ce1fe15ba5b6f4531
Jason Mulvenna <jason.mulvenna@gmail.com>
2013-07-19 19:17:52 +1000

    Put in 100,000 limit on mapping queries

--- src/clj_biosequence/core.clj
+++ src/clj_biosequence/core.clj
@@ -358,21 +358,28 @@
    GI number	            P_GI	     both
    RefSeq Protein	    P_REFSEQ_AC	     both
    RefSeq Nucleotide	    REFSEQ_NT_ID     both
-   WormBase	            WORMBASE_ID	     both"
+   WormBase	            WORMBASE_ID	     both
+
+   There is a 100,000 limit on accessions in a single query imposed by Uniprot."
   [ids from to email]
-  (let [param {:from from :to to :format "tab" 
-               :query (apply str (doall (interpose "," (if (list? ids)
-                                                         ids
-                                                         (list ids)))))}
-        address "http://www.uniprot.org/mapping/"
-        r (client/post address 
-                       {:client-params {"http.useragent" (str "clj-http " email)}
-                        :follow-redirects true
-                        :force-redirects true
-                        :form-params param})]
-    (if-not (= 200 (:status r))
-      (throw (Throwable. (str "Error in mapping request: " (:body r))))
-      (into {} (map #(string/split % #"\t") (rest (string/split (:body r) #"\n")))))))
+  (let [i (if (coll? ids)
+            ids (list ids))]
+    (if (<= (count ids) 100000)
+      (let [param {:from from :to to :format "tab" 
+                   :query (apply str (doall (interpose "," (if (list? ids)
+                                                             ids
+                                                             (list ids)))))}
+            address "http://www.uniprot.org/mapping/"
+            r (client/post address 
+                           {:client-params
+                            {"http.useragent" (str "clj-http " email)}
+                            :follow-redirects true
+                            :force-redirects true
+                            :form-params param})]
+        (if-not (= 200 (:status r))
+          (throw (Throwable. (str "Error in mapping request: " (:body r))))
+          (into {} (map #(string/split % #"\t") (rest (string/split (:body r) #"\n"))))))
+      (throw (Throwable. "No more than 100,000 mappings per query allowed.")))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; utilities
