38418e6412d12efaca3d8987db3322ee155716c3
Jason Mulvenna <jason.mulvenna@gmail.com>
2013-11-02 00:37:15 +1000

    Amino acids masses

--- src/clj_biosequence/alphabet.clj
+++ src/clj_biosequence/alphabet.clj
@@ -20,33 +20,44 @@
    \X {:longname "Any base (A, C, G, T, or U)" :complement \X}})
 
 (def iupacAminoAcids
-  {\A {:three "Ala" :longname "Alanine"}
-   \R {:three "Arg" :longname "Arginine"}
-   \N {:three "Asn" :longname "Asparagine"}
-   \D {:three "Asp" :longname "Aspartic acid"}
-   \C {:three "Cys" :longname "Cysteine"}
-   \Q {:three "Gln" :longname "Glutamine"}
-   \E {:three "Glu" :longname "Glutamic acid"}
-   \G {:three "Gly" :longname "Glycine"}
-   \H {:three "His" :longname "Histidine"}
-   \I {:three "Ile" :longname "Isoleucine"}
-   \L {:three "Leu" :longname "Leucine"}
-   \K {:three "Lys" :longname "Lysine"}
-   \M {:three "Met" :longname "Methionine"}
-   \F {:three "Phe" :longname "Phenylalanine"}
-   \P {:three "Pro" :longname "Proline"}
-   \S {:three "Ser" :longname "Serine"}
-   \T {:three "Thr" :longname "Threonine"}
-   \W {:three "Trp" :longname "Tryptophan"}
-   \Y {:three "Tyr" :longname "Tyrosine"}
-   \V {:three "Val" :longname "Valine"}
-   \B {:three "Asx" :longname "Aspartic acid or Asparagine"}
-   \Z {:three "Glx" :longname "Glutamine or Glutamic acid"}
-   \J {:three "Xle" :longname "Leucine or Isoleucine"}
-   \X {:three "Xaa" :longname "Any amino acid"}
-   \U {:three "Sec" :longname "Selenocysteine"}
-   \O {:three "Pyl" :longname "Pyrrolysine"}
-   \* {:three "Stop" :longname "Stop"}})
+  {\A {:three "Ala" :longname "Alanine" :monoisotopic 71.03711 :average 71.0788}
+   \R {:three "Arg" :longname "Arginine" :monoisotopic 156.10111 :average 156.1875}
+   \N {:three "Asn" :longname "Asparagine" :monoisotopic 114.04293 :average 114.1038}
+   \D {:three "Asp" :longname "Aspartic acid" :monoisotopic 115.02694 :average 115.0886}
+   \C {:three "Cys" :longname "Cysteine" :monoisotopic 103.00919 :average 103.1388}
+   \Q {:three "Gln" :longname "Glutamine" :monoisotopic 128.05858 :average 128.1307}
+   \E {:three "Glu" :longname "Glutamic acid" :monoisotopic 129.04259 :average 129.1155}
+   \G {:three "Gly" :longname "Glycine" :monoisotopic 57.02146 :average 57.0519}
+   \H {:three "His" :longname "Histidine" :monoisotopic 137.05891 :average 137.1411}
+   \I {:three "Ile" :longname "Isoleucine" :monoisotopic 113.08406 :average 113.1594}
+   \L {:three "Leu" :longname "Leucine" :monoisotopic 113.08406 :average 113.1594}
+   \K {:three "Lys" :longname "Lysine" :monoisotopic 128.09496  :average 128.1741}
+   \M {:three "Met" :longname "Methionine" :monoisotopic 131.04049 :average 131.1926}
+   \F {:three "Phe" :longname "Phenylalanine" :monoisotopic 147.06841 :average 147.1766}
+   \P {:three "Pro" :longname "Proline" :monoisotopic 97.05276 :average 97.1167}
+   \S {:three "Ser" :longname "Serine" :monoisotopic 87.03203 :average 87.0782}
+   \T {:three "Thr" :longname "Threonine" :monoisotopic 101.04768 :average 101.1051}
+   \W {:three "Trp" :longname "Tryptophan" :monoisotopic 186.07931 :average 186.2132}
+   \Y {:three "Tyr" :longname "Tyrosine" :monoisotopic 163.06333 :average 163.1760}
+   \V {:three "Val" :longname "Valine" :monoisotopic 99.06841 :average 99.1326}
+   \B {:three "Asx" :longname "Aspartic acid or Asparagine" :monoisotopic nil :average nil}
+   \Z {:three "Glx" :longname "Glutamine or Glutamic acid" :monoisotopic nil :average nil}
+   \J {:three "Xle" :longname "Leucine or Isoleucine" :monoisotopic 113.08406
+       :average 113.1594}
+   \X {:three "Xaa" :longname "Any amino acid" :monoisotopic nil :average nil}
+   \U {:three "Sec" :longname "Selenocysteine" :monoisotopic 150.953636 :average 150.0388}
+   \O {:three "Pyl" :longname "Pyrrolysine" :monoisotopic 237.147727 :average 237.3018}
+   \* {:three "Stop" :longname "Stop" :monoisotopic 0 :average 0}})
+
+;; ; average mod
+;; 'I (+ cys 57.0513)
+;;                'M (+ cys 45.988)
+;; 'O (+ cys 1.007947)
+
+;; ; mono mod
+;; 'I (+ cys 57.021464)
+;;                'M (+ cys 45.988)
+;;                'O (+ cys 1.007947)
 
 (def codon-tables
   {1 {:name "Standard"
@@ -240,7 +251,7 @@
   (#{:iupacNucleicAcids :iupacAminoAcids} k))
 
 (defn get-alphabet
-  "Takes a keyword and returns tha corresponding alphabet hash."
+  "Takes a keyword and returns the corresponding alphabet hash."
   [k]
   ({:iupacAminoAcids iupacAminoAcids
     :iupacNucleicAcids iupacNucleicAcids} k))
