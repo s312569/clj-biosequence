c1a60860410c066b4d93c3d37f31e09de332a005
Jason Mulvenna <jason.mulvenna@gmail.com>
2013-12-15 11:42:46 +1000

    Indexing of sequence files

---
+++ src/clj_biosequence/index.clj
@@ -0,0 +1,105 @@
+(ns clj-biosequence.index
+  (:require [clojure.java.io :as io]
+            [fs.core :as fs]
+            [clojure.string :as string]
+            [clj-biosequence.core :as bs]
+            [taoensso.nippy :as nip])
+  (:import [java.io RandomAccessFile]))
+
+(declare init-file-index write-and-position read-one)
+
+;; reader
+
+(defrecord indexReader [strm]
+
+  java.io.Closeable
+  
+  (close [this]
+    (.close ^java.io.RandomAccessFile (:strm this))))
+
+(defn init-index-reader
+  [strm]
+  (->indexReader strm))
+
+;; indexed file
+
+(defrecord indexFile [path]
+
+  bs/biosequenceFile
+
+  (bs-path [this]
+    (:path this))
+
+  bs/biosequenceIO
+
+  (bs-reader [this]
+    (init-index-reader (RandomAccessFile. (:path this) "rw"))))
+
+(defn init-index-file
+  [path]
+  (->indexFile path))
+
+;; index
+
+(defrecord fileIndex [index]
+
+  bs/biosequenceReader
+
+  (biosequence-seq [this]
+    (map (fn [[o l]]
+           (read-one o l (bs/bs-path (:file this))))
+         (vals (:index this))))
+
+  (get-biosequence [this accession]
+    (let [[o l] (get (:index this) accession)]
+      (read-one o l (bs/bs-path (:file this))))))
+
+(defn save-index
+  [idx]
+  (let [o (str (bs/bs-path (:file idx)) ".idx")]
+    (with-open [out (bs/bs-reader (init-index-file o))]
+      (.write (:strm out) (bs/bs-freeze idx)))))
+
+(defn load-index
+  [file]
+  (let [l (.length (io/file file))
+        bb (byte-array l)]
+    (with-open [r (RandomAccessFile. file "rw")]
+      (.read r bb)
+      (bs/bs-thaw bb))))
+
+;; creation
+
+(defn index-biosequence-file
+  [file]
+  (let [ofile (init-index-file (str (bs/bs-path file) ".bin"))]
+    (with-open [o (bs/bs-reader ofile)]
+      (with-open [i (bs/bs-reader file)]
+        (assoc (->> (bs/biosequence-seq i)
+                    (map #(write-and-position % (:strm o)))
+                    (into {})
+                    init-file-index)
+          :file ofile)))))
+
+;; utility
+
+(defn- init-file-index
+  [h]
+  (->fileIndex h))
+
+(defn- read-one
+  [off len file]
+  (let [bb (byte-array len)]
+    (with-open [r (RandomAccessFile. file "rw")]
+      (.seek r off)
+      (.read r bb)
+      (bs/bs-thaw bb))))
+
+(defn- write-and-position
+  [obj strm]
+  (let [o (nip/freeze obj)
+        off (.getFilePointer strm)]
+    (.write strm o)
+    (vector (bs/accession obj) (list off (count o)))))
+
+
