e8d3c392d0844e62e2511e0bbf33c13b63ce12f2
Jason Mulvenna <jason.mulvenna@gmail.com>
2014-03-20 23:07:27 +1000

    IPS new interface

--- src/clj_biosequence/interproscan.clj
+++ src/clj_biosequence/interproscan.clj
@@ -10,68 +10,9 @@
 
 (declare run-ips)
 
-;; ips go entry
+;; matches
 
-(defrecord interproscanGo [src])
-
-(defn go-component
-  [go]
-  (zf/xml1-> (zip/xml-zip (:src go))
-             :category
-             zf/text))
-
-(defn go-accession [go]
-  (zf/xml1-> (zip/xml-zip (:src go))
-             (zf/attr :id)))
-
-(defn go-description [go]
-  (zf/xml1-> (zip/xml-zip (:src go))
-             :description
-             zf/text))
-
-;; ips entry
-
-(defrecord interproscanEntry [src])
-
-(defn init-ips-entry
-  [src]
-  (->interproscanEntry src))
-
-(defn ips-entry-type
-  [e]
-  (zf/xml1-> (zip/xml-zip (:src e))
-             (zf/attr :type)))
-
-(defn ips-entry-name
-  [e]
-  (zf/xml1-> (zip/xml-zip (:src e))
-             (zf/attr :name)))
-
-(defn ips-entry-accession
-  [e]
-  (zf/xml1-> (zip/xml-zip (:src e))
-             (zf/attr :id)))
-
-(defn ips-entry-start
-  [e]
-  (zf/xml1-> (zip/xml-zip (:src e))
-             :match
-             :location
-             (zf/attr :start)))
-
-(defn ips-entry-end
-  [e]
-  (zf/xml1-> (zip/xml-zip (:src e))
-             :match
-             :location
-             (zf/attr :end)))
-
-(defn ips-go-seq
-  [e]
-  (map #(->interproscanGo (zip/node %))
-       (zf/xml-> (zip/xml-zip (:src e))
-                 :classification
-                 (zf/attr= :class_type "GO"))))
+(defrecord interproscan [src])
 
 ;; ips protein
 
@@ -80,36 +21,23 @@
   bs/Biosequence
 
   (accession [this]
-    (apply str
-           (interpose "_"
-                      (drop-last 2
-                                 (string/split (zf/xml1-> (zip/xml-zip (:src this))
-                                                          (zf/attr :id))
-                                               #"_"))))))
+    (zf/xml1-> (zip/xml-zip (:src this))
+               :xref (zf/attr :id))))
 
-(defn init-ips-protein
-  [src]
-  (->interproscanProtein src))
+(defn match-seq [protein]
+  (map #()))
 
-(defn ips-entry-seq
-  [ip]
-  (map #(init-ips-entry (zip/node %)) (zf/xml-> (zip/xml-zip (:src ip)) :interpro)))
+;; ips searchx
 
-(defn prot-go-terms
-  [ip]
-  (flatten (pmap #(ips-go-seq %)
-                 (ips-entry-seq ip))))
-
-;; ips search
-
-(defrecord interproscanReader [strm]
+(defrecord interproscanReader [strms]
 
   bs/biosequenceReader
 
   (biosequence-seq [this]
-    (->> (:content (xml/parse (:strm this)))
-         (filter #(= :protein (:tag %)))
-         (map #(->interproscanProtein %))))
+    (mapcat #(->> (:content (xml/parse %))
+                  (filter (fn [x] (= :protein (:tag x))))
+                  (map (fn [x] (->interproscanProtein x))))
+            (:strms this)))
 
   (parameters [this]
     ())
@@ -117,59 +45,59 @@
   java.io.Closeable
 
   (close [this]
-    (.close ^java.io.BufferedReader (:strm this))))
+    (doseq [r (:strms this)]
+      (.close ^java.io.BufferedReader r))))
 
-(defrecord interproscanFile [file]
+(defrecord interproscanResult [files]
 
   bs/biosequenceIO
 
   (bs-reader [this]
-    (->interproscanReader (io/reader (:file this))))
-
-  bs/biosequenceFile
-
-  (bs-path [this]
-    (:file this)))
-
-(defn init-ips-result-file
-  [file]
-  (->interproscanFile (fs/absolute-path file)))
+    (->interproscanReader (map io/reader (:files this)))))
 
 ;; ips run
 
 (defn ips
-  [bs seqtype & {:keys [outfile appl lookup goterms] :or {outfile (fs/temp-file "ips")
-                                                          appl '("hmmpfam")
-                                                          lookup true
-                                                          goterms true}}]
-  (if (#{"p" "n"} seqtype)
-    (let [i (bs/biosequence->file bs (fs/temp-file "seq-") :append false
-                              :func (fn [x] (if (not (> (count (bs/bs-seq x)) 10000))
-                                             (bs/fasta-string x))))]
-      (try
-        (run-ips (fs/absolute-path i) (fs/absolute-path outfile)
-                 seqtype appl lookup goterms)
-        (finally (fs/delete i))))
-    (throw (Throwable. (str seqtype " Not supported. Seqtype can be 'p' or 'n' only")))))
+  "Runs interproscan on a list of biosequences."
+  [bsl & {:keys [outfile appl lookup goterms precalc pathways]
+          :or {outfile (fs/temp-file "ips")
+               appl '("pfam")
+               lookup true
+               goterms true
+               precalc false
+               pathways false}}]
+  (->interproscanResult
+   (doall (map #(let [i (bs/biosequence->file % (fs/temp-file "seq-") :append false)]
+                  (try
+                    (run-ips (fs/absolute-path i) (fs/absolute-path outfile)
+                             seqtype appl precalc pathways lookup goterms)
+                    (finally (fs/delete i))))
+               (partition-all 10000 bsl)))))
+
+;; pfam tigrfam - hmmer3-match
+;; prodom
+;; 
 
 ;; utilities
 
 (defn ips-command
-  [i o s a l g]
+  [i o s a p path l g]
   (vec
    (remove nil? (-> (list
-                     "iprscan" "-cli" "-i" i "-o" o "-seqtype" s
+                     "interproscan.sh" "-i" i "-o" o "-seqtype" s "-f" "XML"
+                     "-appl" (apply str (interpose "," a))
+                     (if (not p) "-dp")
+                     (if (not path) "-pa")
                      (if (or l g) "-iprlookup")
-                     (if g "-goterms"))
-                    (concat (interleave (iterate identity "-appl ") a))))))
+                     (if g "-goterms"))))))
 
 (defn- run-ips
-  [i o s a l g]
+  [i o s a p path l g]
   (try
-    (let [ips @(exec/sh (ips-command i o s a l g))]
+    (let [ips @(exec/sh (ips-command i o s a p path l g))]
       (if (= 0 (:exit ips))
-        (->interproscanFile o)
+        o
         (throw (Throwable. (str "Interproscan error: " (:err ips))))))
     (catch Exception e
-      (str "Exception: " (:exception ips))
+      (throw e)
       (fs/delete o))))
