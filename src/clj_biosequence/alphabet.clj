f7990db31c22e4b371a4b4b15fcdc00959effea3
Jason Mulvenna <jasonM@havana0.adqimr.ad.lan>
2014-03-20 13:11:46 +1000

    Added a signalpAminoAcids alphabet to account for signalp's smaller range of allowable amino acids, i.e. any object with this alphabet will replace anything apart from signalp allowable amino acids with 'X'

--- src/clj_biosequence/alphabet.clj
+++ src/clj_biosequence/alphabet.clj
@@ -49,6 +49,29 @@
    \O {:three "Pyl" :longname "Pyrrolysine" :monoisotopic 237.147727 :average 237.3018}
    \* {:three "Stop" :longname "Stop" :monoisotopic 0 :average 0}})
 
+(def signalpAminoAcids
+  {\A {:three "Ala" :longname "Alanine" :monoisotopic 71.03711 :average 71.0788}
+   \R {:three "Arg" :longname "Arginine" :monoisotopic 156.10111 :average 156.1875}
+   \N {:three "Asn" :longname "Asparagine" :monoisotopic 114.04293 :average 114.1038}
+   \D {:three "Asp" :longname "Aspartic acid" :monoisotopic 115.02694 :average 115.0886}
+   \C {:three "Cys" :longname "Cysteine" :monoisotopic 103.00919 :average 103.1388}
+   \Q {:three "Gln" :longname "Glutamine" :monoisotopic 128.05858 :average 128.1307}
+   \E {:three "Glu" :longname "Glutamic acid" :monoisotopic 129.04259 :average 129.1155}
+   \G {:three "Gly" :longname "Glycine" :monoisotopic 57.02146 :average 57.0519}
+   \H {:three "His" :longname "Histidine" :monoisotopic 137.05891 :average 137.1411}
+   \I {:three "Ile" :longname "Isoleucine" :monoisotopic 113.08406 :average 113.1594}
+   \L {:three "Leu" :longname "Leucine" :monoisotopic 113.08406 :average 113.1594}
+   \K {:three "Lys" :longname "Lysine" :monoisotopic 128.09496  :average 128.1741}
+   \M {:three "Met" :longname "Methionine" :monoisotopic 131.04049 :average 131.1926}
+   \F {:three "Phe" :longname "Phenylalanine" :monoisotopic 147.06841 :average 147.1766}
+   \P {:three "Pro" :longname "Proline" :monoisotopic 97.05276 :average 97.1167}
+   \S {:three "Ser" :longname "Serine" :monoisotopic 87.03203 :average 87.0782}
+   \T {:three "Thr" :longname "Threonine" :monoisotopic 101.04768 :average 101.1051}
+   \W {:three "Trp" :longname "Tryptophan" :monoisotopic 186.07931 :average 186.2132}
+   \Y {:three "Tyr" :longname "Tyrosine" :monoisotopic 163.06333 :average 163.1760}
+   \V {:three "Val" :longname "Valine" :monoisotopic 99.06841 :average 99.1326}
+   \X {:three "Xaa" :longname "Any amino acid" :monoisotopic nil :average nil}})
+
 ;; ; average mod
 ;; 'I (+ cys 57.0513)
 ;;                'M (+ cys 45.988)
@@ -248,13 +271,14 @@
   [k]
   "Takes a keyword and returns the keyword if it is a recognised alphabet. Returns
    nil otherwise."
-  (#{:iupacNucleicAcids :iupacAminoAcids} k))
+  (#{:iupacNucleicAcids :iupacAminoAcids :signalpAminoAcids} k))
 
 (defn get-alphabet
   "Takes a keyword and returns the corresponding alphabet hash."
   [k]
   ({:iupacAminoAcids iupacAminoAcids
-    :iupacNucleicAcids iupacNucleicAcids} k))
+    :iupacNucleicAcids iupacNucleicAcids
+    :signalpAminoAcids signalpAminoAcids} k))
 
 (defn alphabet-chars
   "Returns a set of the allowable characters in an alphabet. If not a valid alphabet
@@ -264,3 +288,9 @@
     (let [k (keys (get-alphabet a))]
       (set (concat k (map #(Character/toLowerCase ^Character %) k))))
     (throw (IllegalArgumentException. (str "Not a valid alphabet: " a)))))
+
+(defn alphabet-is-protein
+  "Returns true if alphabet is a protein alphabet."
+  [alphabet]
+  (if (#{:iupacAminoAcids :signalpAminoAcids} alphabet)
+    true false))
