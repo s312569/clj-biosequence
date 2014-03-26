2d5bf8fb802bd0a6632e04d0363c890fa820657c
Jason Mulvenna <jason.mulvenna@gmail.com>
2014-03-26 16:07:11 +1000

    Added lower case letters to the sequence hashes to save time during sequence processing

--- src/clj_biosequence/alphabet.clj
+++ src/clj_biosequence/alphabet.clj
@@ -17,7 +17,24 @@
    \H {:longname "A, T, U, or C (not G)" :complement \D}
    \V {:longname "A, C, or G (not T, not U)" :complement \B}
    \N {:longname "Any base (A, C, G, T, or U)" :complement \N}
-   \X {:longname "Any base (A, C, G, T, or U)" :complement \X}})
+   \X {:longname "Any base (A, C, G, T, or U)" :complement \X}
+   \a {:longname "Adenine" :complement \T}
+   \c {:longname "Cytosine" :complement \G}
+   \g {:longname "Guanine" :complement \C}
+   \t {:longname "Thymine" :complement \A}
+   \u {:longname "Uracil" :complement \A}
+   \r {:longname "Purine (A or G)" :complement \Y}
+   \y {:longname "Pyrimidine (C, T, or U)" :complement \R}
+   \m {:longname "C or A" :complement \K}
+   \k {:longname "T, U, or G" :complement \M}
+   \w {:longname "T, U, or A" :complement \W}
+   \s {:longname "C or G" :complement \S}
+   \b {:longname "C, T, U, or G (not A)" :complement \V}
+   \d {:longname "A, T, U, or G (not C)" :complement \H}
+   \h {:longname "A, T, U, or C (not G)" :complement \D}
+   \v {:longname "A, C, or G (not T, not U)" :complement \B}
+   \n {:longname "Any base (A, C, G, T, or U)" :complement \N}
+   \x {:longname "Any base (A, C, G, T, or U)" :complement \X}})
 
 (def iupacAminoAcids
   {\A {:three "Ala" :longname "Alanine" :monoisotopic 71.03711 :average 71.0788}
@@ -47,7 +64,34 @@
    \X {:three "Xaa" :longname "Any amino acid" :monoisotopic 100.0 :average 100.0}
    \U {:three "Sec" :longname "Selenocysteine" :monoisotopic 150.953636 :average 150.0388}
    \O {:three "Pyl" :longname "Pyrrolysine" :monoisotopic 237.147727 :average 237.3018}
-   \* {:three "Stop" :longname "Stop" :monoisotopic 0 :average 0}})
+   \* {:three "Stop" :longname "Stop" :monoisotopic 0 :average 0}
+   \a {:three "Ala" :longname "Alanine" :monoisotopic 71.03711 :average 71.0788}
+   \r {:three "Arg" :longname "Arginine" :monoisotopic 156.10111 :average 156.1875}
+   \n {:three "Asn" :longname "Asparagine" :monoisotopic 114.04293 :average 114.1038}
+   \d {:three "Asp" :longname "Aspartic acid" :monoisotopic 115.02694 :average 115.0886}
+   \c {:three "Cys" :longname "Cysteine" :monoisotopic 103.00919 :average 103.1388}
+   \q {:three "Gln" :longname "Glutamine" :monoisotopic 128.05858 :average 128.1307}
+   \e {:three "Glu" :longname "Glutamic acid" :monoisotopic 129.04259 :average 129.1155}
+   \g {:three "Gly" :longname "Glycine" :monoisotopic 57.02146 :average 57.0519}
+   \h {:three "His" :longname "Histidine" :monoisotopic 137.05891 :average 137.1411}
+   \i {:three "Ile" :longname "Isoleucine" :monoisotopic 113.08406 :average 113.1594}
+   \l {:three "Leu" :longname "Leucine" :monoisotopic 113.08406 :average 113.1594}
+   \k {:three "Lys" :longname "Lysine" :monoisotopic 128.09496  :average 128.1741}
+   \m {:three "Met" :longname "Methionine" :monoisotopic 131.04049 :average 131.1926}
+   \f {:three "Phe" :longname "Phenylalanine" :monoisotopic 147.06841 :average 147.1766}
+   \p {:three "Pro" :longname "Proline" :monoisotopic 97.05276 :average 97.1167}
+   \s {:three "Ser" :longname "Serine" :monoisotopic 87.03203 :average 87.0782}
+   \t {:three "Thr" :longname "Threonine" :monoisotopic 101.04768 :average 101.1051}
+   \w {:three "Trp" :longname "Tryptophan" :monoisotopic 186.07931 :average 186.2132}
+   \y {:three "Tyr" :longname "Tyrosine" :monoisotopic 163.06333 :average 163.1760}
+   \v {:three "Val" :longname "Valine" :monoisotopic 99.06841 :average 99.1326}
+   \b {:three "Asx" :longname "Aspartic acid or Asparagine" :monoisotopic nil :average nil}
+   \z {:three "Glx" :longname "Glutamine or Glutamic acid" :monoisotopic nil :average nil}
+   \j {:three "Xle" :longname "Leucine or Isoleucine" :monoisotopic 113.08406
+       :average 113.1594}
+   \x {:three "Xaa" :longname "Any amino acid" :monoisotopic 100.0 :average 100.0}
+   \u {:three "Sec" :longname "Selenocysteine" :monoisotopic 150.953636 :average 150.0388}
+   \o {:three "Pyl" :longname "Pyrrolysine" :monoisotopic 237.147727 :average 237.3018}})
 
 (def signalpAminoAcids
   {\A {:three "Ala" :longname "Alanine" :monoisotopic 71.03711 :average 71.0788 :pka 0}
@@ -70,7 +114,28 @@
    \W {:three "Trp" :longname "Tryptophan" :monoisotopic 186.07931 :average 186.2132 :pka 0}
    \Y {:three "Tyr" :longname "Tyrosine" :monoisotopic 163.06333 :average 163.1760 :pka 10.0}
    \V {:three "Val" :longname "Valine" :monoisotopic 99.06841 :average 99.1326 :pka 0}
-   \X {:three "Xaa" :longname "Any amino acid" :monoisotopic nil :average nil :pka 0}})
+   \X {:three "Xaa" :longname "Any amino acid" :monoisotopic nil :average nil :pka 0}
+   \a {:three "Ala" :longname "Alanine" :monoisotopic 71.03711 :average 71.0788 :pka 0}
+   \r {:three "Arg" :longname "Arginine" :monoisotopic 156.10111 :average 156.1875 :pka 12.4}
+   \n {:three "Asn" :longname "Asparagine" :monoisotopic 114.04293 :average 114.1038 :pka 0}
+   \d {:three "Asp" :longname "Aspartic acid" :monoisotopic 115.02694 :average 115.0886 :pka 3.86}
+   \c {:three "Cys" :longname "Cysteine" :monoisotopic 103.00919 :average 103.1388 :pka 8.33}
+   \q {:three "Gln" :longname "Glutamine" :monoisotopic 128.05858 :average 128.1307 :pka 0}
+   \e {:three "Glu" :longname "Glutamic acid" :monoisotopic 129.04259 :average 129.1155 :pka 4.25}
+   \g {:three "Gly" :longname "Glycine" :monoisotopic 57.02146 :average 57.0519 :pka 0}
+   \h {:three "His" :longname "Histidine" :monoisotopic 137.05891 :average 137.1411 :pka 6.0}
+   \i {:three "Ile" :longname "Isoleucine" :monoisotopic 113.08406 :average 113.1594 :pka 0}
+   \l {:three "Leu" :longname "Leucine" :monoisotopic 113.08406 :average 113.1594 :pka 0}
+   \k {:three "Lys" :longname "Lysine" :monoisotopic 128.09496  :average 128.1741 :pka 10.5}
+   \m {:three "Met" :longname "Methionine" :monoisotopic 131.04049 :average 131.1926 :pka 0}
+   \f {:three "Phe" :longname "Phenylalanine" :monoisotopic 147.06841 :average 147.1766 :pka 0}
+   \p {:three "Pro" :longname "Proline" :monoisotopic 97.05276 :average 97.1167 :pka 0}
+   \s {:three "Ser" :longname "Serine" :monoisotopic 87.03203 :average 87.0782 :pka 0}
+   \t {:three "Thr" :longname "Threonine" :monoisotopic 101.04768 :average 101.1051 :pka 0}
+   \w {:three "Trp" :longname "Tryptophan" :monoisotopic 186.07931 :average 186.2132 :pka 0}
+   \y {:three "Tyr" :longname "Tyrosine" :monoisotopic 163.06333 :average 163.1760 :pka 10.0}
+   \v {:three "Val" :longname "Valine" :monoisotopic 99.06841 :average 99.1326 :pka 0}
+   \x {:three "Xaa" :longname "Any amino acid" :monoisotopic nil :average nil :pka 0}})
 
 ;; ; average mod
 ;; 'I (+ cys 57.0513)
@@ -279,15 +344,6 @@
   ({:iupacAminoAcids iupacAminoAcids
     :iupacNucleicAcids iupacNucleicAcids
     :signalpAminoAcids signalpAminoAcids} k))
-
-(defn alphabet-chars
-  "Returns a set of the allowable characters in an alphabet. If not a valid alphabet
-   throws an IllegalArgumentException."
-  [a]
-  (if (alphabet? a)
-    (let [k (keys (get-alphabet a))]
-      (set (concat k (map #(Character/toLowerCase ^Character %) k))))
-    (throw (IllegalArgumentException. (str "Not a valid alphabet: " a)))))
 
 (defn alphabet-is-protein
   "Returns true if alphabet is a protein alphabet."
