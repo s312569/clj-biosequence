1a97565545ac78f8fb185ddeff8c130e0b47ffb6
Jason Mulvenna <jason.mulvenna@gmail.com>
2014-04-10 18:27:30 +1000

    Moved translation tables to resource

---
+++ resources/codon-table.clj
@@ -0,0 +1,162 @@
+{1 {:name "Standard"
+    :ncbieaa  [\F \F \L \L \S \S \S \S \Y \Y \* \* \C \C \* \W \L \L \L \L \P
+               \P \P \P \H \H \Q \Q \R \R \R \R \I \I \I \M \T \T \T \T \N \N
+               \K \K \S \S \R \R \V \V \V \V \A \A \A \A \D \D \E \E \G \G \G
+               \G]
+    :sncbieaa [\- \- \- \M \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \M \-
+               \- \- \- \- \- \- \- \- \- \- \- \- \- \- \M \- \- \- \- \- \-
+               \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \-
+               \-]}
+ 2 {:name "Vertebrate Mitochondrial"
+    :ncbieaa  [\F \F \L \L \S \S \S \S \Y \Y \* \* \C \C \W \W \L \L \L \L \P
+               \P \P \P \H \H \Q \Q \R \R \R \R \I \I \M \M \T \T \T \T \N \N
+               \K \K \S \S \* \* \V \V \V \V \A \A \A \A \D \D \E \E \G \G \G
+               \G]
+    :sncbieaa [\- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \-
+               \- \- \- \- \- \- \- \- \- \- \- \M \M \M \M \- \- \- \- \- \-
+               \- \- \- \- \- \- \- \- \- \M \- \- \- \- \- \- \- \- \- \- \-
+               \-]}
+ 3 {:name "Yeast Mitochondrial"
+    :ncbieaa  [\F \F \L \L \S \S \S \S \Y \Y \* \* \C \C \W \W \T \T \T \T \P
+               \P \P \P \H \H \Q \Q \R \R \R \R \I \I \M \M \T \T \T \T \N \N
+               \K \K \S \S \R \R \V \V \V \V \A \A \A \A \D \D \E \E \G \G \G
+               \G]
+    :sncbieaa [\- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \-
+               \- \- \- \- \- \- \- \- \- \- \- \- \- \M \M \- \- \- \- \- \-
+               \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \-
+               \-]}
+ 4 {:name "Mold Mitochondrial; Protozoan Mitochondrial; Coelenterate Mitochondrial; Mycoplasma; Spiroplasma"
+    :ncbieaa  [\F \F \L \L \S \S \S \S \Y \Y \* \* \C \C \W \W \L \L \L \L \P
+               \P \P \P \H \H \Q \Q \R \R \R \R \I \I \I \M \T \T \T \T \N \N
+               \K \K \S \S \R \R \V \V \V \V \A \A \A \A \D \D \E \E \G \G \G
+               \G]
+    :sncbieaa [\- \- \M \M \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \M \-
+               \- \- \- \- \- \- \- \- \- \- \- \M \M \M \M \- \- \- \- \- \-
+               \- \- \- \- \- \- \- \- \- \M \- \- \- \- \- \- \- \- \- \- \-
+               \-]}
+ 5 {:name "Invertebrate Mitochondrial"
+    :ncbieaa  [\F \F \L \L \S \S \S \S \Y \Y \* \* \C \C \W \W \L \L \L \L \P
+               \P \P \P \H \H \Q \Q \R \R \R \R \I \I \M \M \T \T \T \T \N \N
+               \K \K \S \S \S \S \V \V \V \V \A \A \A \A \D \D \E \E \G \G \G
+               \G]
+    :sncbieaa [\- \- \- \M \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \-
+               \- \- \- \- \- \- \- \- \- \- \- \M \M \M \M \- \- \- \- \- \-
+               \- \- \- \- \- \- \- \- \- \M \- \- \- \- \- \- \- \- \- \- \-
+               \-]}
+ 6 {:name "Ciliate Nuclear; Dasycladacean Nuclear; Hexamita Nuclear"
+    :ncbieaa  [\F \F \L \L \S \S \S \S \Y \Y \Q \Q \C \C \* \W \L \L \L \L \P
+               \P \P \P \H \H \Q \Q \R \R \R \R \I \I \I \M \T \T \T \T \N \N
+               \K \K \S \S \R \R \V \V \V \V \A \A \A \A \D \D \E \E \G \G \G
+               \G]
+    :sncbieaa [\- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \-
+               \- \- \- \- \- \- \- \- \- \- \- \- \- \- \M \- \- \- \- \- \-
+               \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \-
+               \-]}
+ 9 {:name "Echinoderm Mitochondrial; Flatworm Mitochondrial"
+    :ncbieaa  [\F \F \L \L \S \S \S \S \Y \Y \* \* \C \C \W \W \L \L \L \L \P
+               \P \P \P \H \H \Q \Q \R \R \R \R \I \I \I \M \T \T \T \T \N \N
+               \N \K \S \S \S \S \V \V \V \V \A \A \A \A \D \D \E \E \G \G \G
+               \G]
+    :sncbieaa [\- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \-
+               \- \- \- \- \- \- \- \- \- \- \- \- \- \- \M \- \- \- \- \- \-
+               \- \- \- \- \- \- \- \- \- \M \- \- \- \- \- \- \- \- \- \- \-
+               \-]}
+ 10 {:name "Euplotid Nuclear" ,
+     :ncbieaa  [\F \F \L \L \S \S \S \S \Y \Y \* \* \C \C \C \W \L \L \L \L \P
+                \P \P \P \H \H \Q \Q \R \R \R \R \I \I \I \M \T \T \T \T \N \N
+                \K \K \S \S \R \R \V \V \V \V \A \A \A \A \D \D \E \E \G \G \G
+                \G]
+     :sncbieaa [\- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \-
+                \- \- \- \- \- \- \- \- \- \- \- \- \- \- \M \- \- \- \- \- \-
+                \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \-
+                \-]}
+ 11 {:name "Bacterial, Archaeal and Plant Plastid"
+     :ncbieaa  [\F \F \L \L \S \S \S \S \Y \Y \* \* \C \C \* \W \L \L \L \L \P
+                \P \P \P \H \H \Q \Q \R \R \R \R \I \I \I \M \T \T \T \T \N \N
+                \K \K \S \S \R \R \V \V \V \V \A \A \A \A \D \D \E \E \G \G \G
+                \G]
+     :sncbieaa [\- \- \- \M \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \M \-
+                \- \- \- \- \- \- \- \- \- \- \- \M \M \M \M \- \- \- \- \- \-
+                \- \- \- \- \- \- \- \- \- \M \- \- \- \- \- \- \- \- \- \- \-
+                \-]}
+ 12 {:name "Alternative Yeast Nuclear"
+     :ncbieaa  [\F \F \L \L \S \S \S \S \Y \Y \* \* \C \C \* \W \L \L \L \S \P
+                \P \P \P \H \H \Q \Q \R \R \R \R \I \I \I \M \T \T \T \T \N \N
+                \K \K \S \S \R \R \V \V \V \V \A \A \A \A \D \D \E \E \G \G \G
+                \G]
+     :sncbieaa [\- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \M \-
+                \- \- \- \- \- \- \- \- \- \- \- \- \- \- \M \- \- \- \- \- \-
+                \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \-
+                \-]}
+ 13 {:name "Ascidian Mitochondrial"
+     :ncbieaa  [\F \F \L \L \S \S \S \S \Y \Y \* \* \C \C \W \W \L \L \L \L \P
+                \P \P \P \H \H \Q \Q \R \R \R \R \I \I \M \M \T \T \T \T \N \N
+                \K \K \S \S \G \G \V \V \V \V \A \A \A \A \D \D \E \E \G \G \G
+                \G]
+     :sncbieaa [\- \- \- \M \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \-
+                \- \- \- \- \- \- \- \- \- \- \- \- \- \M \M \- \- \- \- \- \-
+                \- \- \- \- \- \- \- \- \- \M \- \- \- \- \- \- \- \- \- \- \-
+                \-]}
+ 14 {:name "Alternative Flatworm Mitochondrial"
+     :ncbieaa  [\F \F \L \L \S \S \S \S \Y \Y \Y \* \C \C \W \W \L \L \L \L \P
+                \P \P \P \H \H \Q \Q \R \R \R \R \I \I \I \M \T \T \T \T \N \N
+                \N \K \S \S \S \S \V \V \V \V \A \A \A \A \D \D \E \E \G \G \G
+                \G]
+     :sncbieaa [\- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \-
+                \- \- \- \- \- \- \- \- \- \- \- \- \- \- \M \- \- \- \- \- \-
+                \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \-
+                \-]}
+ 15 {:name "Blepharisma Macronuclear"
+     :ncbieaa  [\F \F \L \L \S \S \S \S \Y \Y \* \Q \C \C \* \W \L \L \L \L \P
+                \P \P \P \H \H \Q \Q \R \R \R \R \I \I \I \M \T \T \T \T \N \N
+                \K \K \S \S \R \R \V \V \V \V \A \A \A \A \D \D \E \E \G \G \G
+                \G]
+     :sncbieaa [\- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \-
+                \- \- \- \- \- \- \- \- \- \- \- \- \- \- \M \- \- \- \- \- \-
+                \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \-
+                \-]}
+ 16 {:name "Chlorophycean Mitochondrial"
+     :ncbieaa  [\F \F \L \L \S \S \S \S \Y \Y \* \L \C \C \* \W \L \L \L \L \P
+                \P \P \P \H \H \Q \Q \R \R \R \R \I \I \I \M \T \T \T \T \N \N
+                \K \K \S \S \R \R \V \V \V \V \A \A \A \A \D \D \E \E \G \G \G
+                \G]
+     :sncbieaa [\- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \-
+                \- \- \- \- \- \- \- \- \- \- \- \- \- \- \M \- \- \- \- \- \-
+                \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \-
+                \-]}
+ 21 {:name "Trematode Mitochondrial"
+     :ncbieaa  [\F \F \L \L \S \S \S \S \Y \Y \* \* \C \C \W \W \L \L \L \L \P
+                \P \P \P \H \H \Q \Q \R \R \R \R \I \I \M \M \T \T \T \T \N \N
+                \N \K \S \S \S \S \V \V \V \V \A \A \A \A \D \D \E \E \G \G \G
+                \G]
+     :sncbieaa [\- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \-
+                \- \- \- \- \- \- \- \- \- \- \- \- \- \- \M \- \- \- \- \- \-
+                \- \- \- \- \- \- \- \- \- \M \- \- \- \- \- \- \- \- \- \- \-
+                \-]}
+ 22 {:name "Scenedesmus obliquus Mitochondrial"
+     :ncbieaa  [\F \F \L \L \S \S \* \S \Y \Y \* \L \C \C \* \W \L \L \L \L \P
+                \P \P \P \H \H \Q \Q \R \R \R \R \I \I \I \M \T \T \T \T \N \N
+                \K \K \S \S \R \R \V \V \V \V \A \A \A \A \D \D \E \E \G \G \G
+                \G]
+     :sncbieaa [\- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \-
+                \- \- \- \- \- \- \- \- \- \- \- \- \- \- \M \- \- \- \- \- \-
+                \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \-
+                \-]}
+ 23 {:name "Thraustochytrium Mitochondrial"
+     :ncbieaa  [\F \F \* \L \S \S \S \S \Y \Y \* \* \C \C \* \W \L \L \L \L \P
+                \P \P \P \H \H \Q \Q \R \R \R \R \I \I \I \M \T \T \T \T \N \N
+                \K \K \S \S \R \R \V \V \V \V \A \A \A \A \D \D \E \E \G \G \G
+                \G]
+     :sncbieaa [\- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \-
+                \- \- \- \- \- \- \- \- \- \- \- \M \- \- \M \- \- \- \- \- \-
+                \- \- \- \- \- \- \- \- \- \M \- \- \- \- \- \- \- \- \- \- \-
+                \-]}
+ 24 {:name "Pterobranchia Mitochondrial"
+     :ncbieaa  [\F \F \L \L \S \S \S \S \Y \Y \* \* \C \C \W \W \L \L \L \L \P
+                \P \P \P \H \H \Q \Q \R \R \R \R \I \I \I \M \T \T \T \T \N \N
+                \K \K \S \S \S \K \V \V \V \V \A \A \A \A \D \D \E \E \G \G \G
+                \G]
+     :sncbieaa [\- \- \- \M \- \- \- \- \- \- \- \- \- \- \- \- \- \- \- \M \-
+                \- \- \- \- \- \- \- \- \- \- \- \- \- \- \M \- \- \- \- \- \-
+                \- \- \- \- \- \- \- \- \- \M \- \- \- \- \- \- \- \- \- \- \-
+                \-]}}
