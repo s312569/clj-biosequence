1751b6cc1c61e724c779565351aca961fd39e2e2
Jason Mulvenna <jason.mulvenna@gmail.com>
2015-01-13 19:37:04 +1000

    Documentation

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-biosequence "0.2.1"
+(defproject clj-biosequence "0.2.2"
   :description "Library for the manipulation of biological sequences."
   :url ""
   :license {:name "Eclipse Public License"
