1522fbab1512071d9da17b0f8b779d7be27686ed
s312569 <jason.mulvenna@gmail.com>
2015-01-15 11:43:41 +1000

    Bug fix

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-biosequence "0.2.2"
+(defproject clj-biosequence "0.2.3"
   :description "Library for the manipulation of biological sequences."
   :url ""
   :license {:name "Eclipse Public License"
