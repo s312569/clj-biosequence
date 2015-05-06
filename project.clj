1747616d5bd00a0b5d15d000e9928ae0dc5f7e3d
Jason Mulvenna <jason.mulvenna@gmail.com>
2015-05-06 22:12:29 +1000

    Uniprot interval bug fixes

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-biosequence "0.3.0"
+(defproject clj-biosequence "0.3.1"
   :description "Library for the manipulation of biological sequences."
   :url ""
   :license {:name "Eclipse Public License"
