f7eb4c656554111f4c1aa04a7c514467d37f137b
Jason Mulvenna <jason.mulvenna@gmail.com>
2016-02-14 13:19:19 +1000

    Moved up to clojure 1.8

--- project.clj
+++ project.clj
@@ -4,19 +4,21 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :repositories [["biojava" "http://www.biojava.org/download/maven/"]]
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.xml "0.0.8"]
                  [org.clojure/data.zip "0.1.1"]
-                 [org.apache.commons/commons-compress "1.9"]
-                 [com.taoensso/nippy "2.7.1"]
-                 [com.velisco/tagged "0.3.4"]
-                 [clj-http "1.0.1"]
-                 [iota "1.1.2"]
-                 [clj-time "0.9.0"]
+                 [org.apache.commons/commons-compress "1.10"]
+                 [com.taoensso/nippy "2.10.0"]
+                 [com.velisco/tagged "0.4.0"]
+                 [clj-http "2.0.1"]
+                 [iota "1.1.3"]
+                 [clj-time "0.11.0"]
                  [fs "1.3.3"]
-                 [org.clojars.hozumi/clj-commons-exec "1.1.0"]]
+                 [org.clojars.hozumi/clj-commons-exec "1.1.0"]
+                 [org.clojure/tools.nrepl "0.2.12"]]
   :resource-paths ["shared" "resources"]
-  :plugins [[codox "0.8.10"]]
+  :plugins [[codox "0.8.10"]
+            [cider/cider-nrepl "0.11.0-SNAPSHOT"]]
   :codox {:src-dir-uri "https://github.com/s312569/clj-biosequence/blob/master/"
           :src-linenum-anchor-prefix "L"}
   :repl-options {:init (set! *print-length* 100)}
